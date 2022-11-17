116a809bbcf8870896c9047b15f9992d0bbae781
Alexander Selzer <alexander.selzer@tuwien.ac.at>
2022-11-17 19:41:16 +0100

    add monthly and yearly api

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 
 ```python
 from src.smartmeter import Smartmeter
+import datetime
 
 sc = Smartmeter('USERNAME', 'PASSWORD')
 
@@ -14,11 +15,17 @@ metering_points = sc.get_all_metering_points()
 
 for mp in metering_points:
     sc.get_consumption_month(mp['meteringPointId'], 2022, 11)
-    sc.get_consumption_day(mp['meteringPointId'], 2022, 11, 16)
+    
+    
+    sc.get_consumption_day(mp['meteringPointId'],
+                           datetime.date.today() - datetime.timedelta(days=1))
+
+    # [QuarterHourlyConsumption(metered_value=0.044, estimated_value=None, grid_usage_leftover_values=None, self_coverage_values=None, joint_tenancy_proportion_values=None, metered_peak_demand=0.176, estimated_peak_demand=None, start=datetime.datetime(2022, 11, 16, 0, 0), end=datetime.datetime(2022, 11, 16, 0, 15)),
+    # QuarterHourlyConsumption(metered_value=0.107, estimated_value=None, grid_usage_leftover_values=None, self_coverage_values=None, joint_tenancy_proportion_values=None, metered_peak_demand=0.428, estimated_peak_demand=None, start=datetime.datetime(2022, 11, 16, 0, 15), end=datetime.datetime(2022, 11, 16, 0, 30)),
+    # ...
 
 ```
 
 ## TODO
 
-* convert day/month data to a more readable format
 * Home Assistant integration
--- src/smartmeter.py
+++ src/smartmeter.py
@@ -1,15 +1,50 @@
 import requests
 import logging
+from dataclasses import dataclass
+from datetime import date, datetime, timedelta
 
 API_BASE = 'https://smartmeter.netz-noe.at'
 API_LOGIN = '/orchestration/Authentication/Login'
 API_BASIC_INFO = '/orchestration/User/GetBasicInfo'
 API_GET_ACCOUNTS = '/orchestration/User/GetAccountIdByBussinespartnerId'
 API_GET_METERING_POINTS = '/orchestration/User/GetMeteringPointByAccountId'
+API_GET_CONSUMPTION_YEAR = '/orchestration/ConsumptionRecord/Year'
 API_GET_CONSUMPTION_MONTH = '/orchestration/ConsumptionRecord/Month'
-API_GET_CONSUMPTION_DAY  = '/orchestration/ConsumptionRecord/Day'
+API_GET_CONSUMPTION_DAY = '/orchestration/ConsumptionRecord/Day'
 
 API_EXTEND_SESSION = '/orchestration/Authentication/ExtendSessionLifetime'
+
+
+@dataclass
+class Consumption:
+    metered_value: float
+    estimated_value: float
+    grid_usage_leftover_values: float
+    self_coverage_values: float
+    joint_tenancy_proportion_values: float
+    metered_peak_demand: float
+    estimated_peak_demand: float
+
+
+@dataclass
+class QuarterHourlyConsumption(Consumption):
+    start: datetime
+    end: datetime
+
+
+@dataclass
+class DailyConsumption(Consumption):
+    day: date
+
+
+@dataclass
+class MonthlyConsumption(Consumption):
+    start: date
+
+
+@dataclass
+class YearlyConsumption(Consumption):
+    year: str
 
 
 class Smartmeter:
@@ -71,21 +106,74 @@ class Smartmeter:
             metering_points = metering_points + self.get_metering_points_for_account(account_id)
         return metering_points
 
+    @staticmethod
+    def _make_consumption_instance(api_result, i):
+        metered_value = api_result['meteredValues'][i]
+        estimated_value = api_result['estimatedValues'][i]
+        grid_usage_leftover_value = api_result['gridUsageLeftoverValues'][i]
+        self_coverage_value = api_result['selfCoverageValues'][i]
+        joint_tenancy_proportion_value = api_result['jointTenancyProportionValues'][i]
+        metered_peak_demand = api_result['meteredPeakDemands'][i]
+        estimated_peak_demand = api_result['estimatedPeakDemands'][i]
+
+        return Consumption(metered_value, estimated_value, grid_usage_leftover_value,
+                           self_coverage_value, joint_tenancy_proportion_value,
+                           metered_peak_demand, estimated_peak_demand)
+
+    def get_consumption_year(self, meter_id, year):
+        response = self.session.get(API_BASE + API_GET_CONSUMPTION_MONTH,
+                                    params={'meterId': meter_id, 'year': year})
+        if response.status_code == 200:
+            result = []
+            api_result = response.json()
+            times = [datetime.fromisoformat(d) if d is not None else None for d in api_result['peakDemandTimes']]
+            for i, t in enumerate(times):
+                c = self._make_consumption_instance(api_result, i)
+
+                result.append(MonthlyConsumption(c.metered_value, c.estimated_value, c.grid_usage_leftover_values,
+                                                 c.self_coverage_values, c.joint_tenancy_proportion_values,
+                                                 c.metered_peak_demand, c.estimated_peak_demand, t))
+            return result
+        else:
+            raise SmartmeterError(response.text)
+
     def get_consumption_month(self, meter_id, year, month):
         response = self.session.get(API_BASE + API_GET_CONSUMPTION_MONTH,
                                     params={'meterId': meter_id, 'year': year, 'month': month})
         if response.status_code == 200:
-            return response.json()
+            result = []
+            api_result = response.json()
+            times = [datetime.fromisoformat(d) if d is not None else None for d in api_result['peakDemandTimes']]
+            for i, t in enumerate(times):
+                c = self._make_consumption_instance(api_result, i)
+
+                result.append(DailyConsumption(c.metered_value, c.estimated_value, c.grid_usage_leftover_values,
+                                               c.self_coverage_values, c.joint_tenancy_proportion_values,
+                                               c.metered_peak_demand, c.estimated_peak_demand, t))
+            return result
         else:
             raise SmartmeterError(response.text)
 
-    def get_consumption_day(self, meter_id, year, month, day):
+    def get_consumption_day(self, meter_id, day: date):
         response = self.session.get(API_BASE + API_GET_CONSUMPTION_DAY,
-                                    params={'meterId': meter_id, 'day': f'{year}-{month}-{day}'})
+                                    params={'meterId': meter_id, 'day': f'{day.year}-{day.month}-{day.day}'})
         if response.status_code == 200:
-            return response.json()
+            result = []
+            api_result = response.json()
+            times = [datetime.fromisoformat(d) if d is not None else None for d in api_result['peakDemandTimes']]
+            for i, t in enumerate(times):
+                c = self._make_consumption_instance(api_result, i)
+                time_start = t - timedelta(minutes=15)
+                time_end = t
+
+                result.append(QuarterHourlyConsumption(c.metered_value, c.estimated_value, c.grid_usage_leftover_values,
+                                                       c.self_coverage_values, c.joint_tenancy_proportion_values,
+                                                       c.metered_peak_demand, c.estimated_peak_demand,
+                                                       time_start, time_end))
+            return result
         else:
             raise SmartmeterError(response.text)
+
 
 class SmartmeterError(Exception):
     pass
